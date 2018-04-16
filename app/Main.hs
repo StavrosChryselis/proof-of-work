f0f9a5ed5a2c84c79cfe31d09ca6c0ea61ac375c
maverick <stavrikios@gmail.com>
2018-04-16 21:10:30 +0300

    Change block from static to dynamic (block + previous hash)

--- app/Main.hs
+++ app/Main.hs
@@ -12,22 +12,30 @@ import Data.Text (pack)
 import System.Microtimer (time)
 import Control.Exception (evaluate)
 import Data.ByteString.Base16 (encode)
+import Data.ByteString (ByteString)
+import qualified Data.ByteString as B
+
+stressTest :: ByteString -> Integer -> IO ()
+stressTest block difficultyBits
+  | difficultyBits > 32 = pure ()
+  | otherwise = do
+    let difficulty = 2^difficultyBits
+    putStrLn $ "Difficulty: " ++ show difficulty ++ " (" ++ show difficultyBits ++ " bits)"
+    putStrLn "Starting search.."
+    (elapsedTime, result) <- time $ evaluate $ proofOfWork block difficultyBits
+    case result of
+        Nothing -> putStrLn $ "Failed after " ++ show maxNonce ++ "(max_nonce) tries"
+        Just (nonce, hashResult) -> do
+            let hashPower = fromIntegral nonce / elapsedTime
+            putStrLn $ "Success with nonce " ++ show nonce
+            putStrLn $ "Hash is " ++ show (encode hashResult)
+            putStrLn $ "Hashing Power is " ++ show hashPower ++ " hashes per second"
+            putStrLn $ "Elapsed Time is " ++ show elapsedTime ++ " seconds"
+            putStrLn ""
+            stressTest (block `B.append` hashResult) (difficultyBits+1)
 
 main :: IO ()
 main = do
     [genesisString] <- getArgs
     let genesisBlock = encodeUtf8 $ pack genesisString
-    forM_ [1..32] $ \ difficultyBits -> do
-        let difficulty = 2^difficultyBits
-        putStrLn $ "Difficulty: " ++ show difficulty ++ " (" ++ show difficultyBits ++ " bits)"
-        putStrLn "Starting search.."
-        (elapsedTime, result) <- time $ evaluate $ proofOfWork genesisBlock difficultyBits
-        case result of
-            Nothing -> putStrLn $ "Failed after " ++ show maxNonce ++ "(max_nonce) tries"
-            Just (nonce, hashResult) -> do
-                let hashPower = fromIntegral nonce / elapsedTime
-                putStrLn $ "Success with nonce " ++ show nonce
-                putStrLn $ "Hash is " ++ show (encode hashResult)
-                putStrLn $ "Hashing Power is " ++ show hashPower ++ " hashes per second"
-        putStrLn $ "Elapsed Time is " ++ show elapsedTime ++ " seconds"
-        putStrLn ""
+    stressTest genesisBlock 1
